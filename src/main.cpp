48432c4b9c1fe0cf1b06db1536a56fddf97453be
liad <programmeraxolotl@gmail.com>
2026-01-22 15:17:58 +0200

    Moved to GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,126 @@
-#include <SDL2/SDL.h>
-#include <gainput/gainput.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-// Define your user buttons somewhere global
-enum Button
-{
-    ButtonConfirm
+#include <iostream>
+#include <cmath>
+
+#include <rendering/Shader.hpp>
+#include <rendering/VAO.h>
+#include <rendering/VBO.h>
+#include <rendering/EBO.h>
+
+
+// Vertices coordinates
+GLfloat vertices[] =
+{ //               COORDINATES                  /     COLORS           //
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
 };
 
-constexpr int SCREEN_WIDTH = 640;
-constexpr int SCREEN_HEIGHT = 480;
-
-int main(int argc, char* argv[])
+// Indices for vertices order
+GLuint indices[] =
 {
-    // Setup window
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow(
-        "Input",
-        SCREEN_WIDTH, SCREEN_HEIGHT,
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOW_SHOWN);
+	0, 3, 5, // Lower left triangle
+	3, 2, 4, // Lower right triangle
+	5, 4, 1 // Upper triangle
+};
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
 
-    // Setting up Gainput
-    gainput::InputManager manager;
-    const gainput::DeviceId mouseId = manager.CreateDevice<gainput::InputDeviceMouse>();
-    manager.SetDisplaySize(SCREEN_WIDTH, SCREEN_HEIGHT);
-    gainput::InputMap map(manager);
-    map.MapBool(ButtonConfirm, mouseId, gainput::MouseButtonLeft);
 
-    SDL_Event event;
-    bool game_running = true;
-    while (game_running)
-    {
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
-                game_running = false;
-            }
-        }
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
 
-        SDL_SetRenderDrawColor(renderer ,0,0,0,255);
-        SDL_RenderClear(renderer);
+	// Tell GLFW what version of OpenGL we are using
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-        // Call every frame
-        manager.Update();
-        // May have to call platform-specific event-handling functions here.
-        // Check button state
-        if (map.GetBoolWasDown(ButtonConfirm))
-        {
-            SDL_Rect rect = {100, 100, 100, 100};
-            SDL_RenderDrawRect(renderer, &rect);
-        }
-        SDL_RenderPresent(renderer);
-    }
-    SDL_Quit();
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
+
+
+
+	// Generates Shader object using shaders defualt.vert and default.frag
+	Shader shaderProgram("resource/shaders/default.vert", "resource/shaders/default.frag");
+
+
+
+	// Generates Vertex Array Object and binds it
+	VAO VAO1;
+	VAO1.bind();
+
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO VBO1(vertices, sizeof(vertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO EBO1(indices, sizeof(indices));
+
+	// Links VBO attributes such as coordinates and colors to VAO
+	VAO1.link_attributes(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.link_attributes(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	// Unbind all to prevent accidentally modifying them
+	VAO1.unbind();
+	VBO1.unbind();
+	EBO1.unbind();
+
+	// Gets ID of uniform called "scale"
+	GLuint uniID = glGetUniformLocation(shaderProgram.id, "scale");
+
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// Clean the back buffer and assign the new color to it
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Tell OpenGL which Shader Program we want to use
+		shaderProgram.activate();
+		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
+		glUniform1f(uniID, 0.5f);
+		// Bind the VAO so OpenGL knows to use it
+		VAO1.bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+
+
+	// Delete all the objects we've created
+	VAO1.destroy();
+	VBO1.destroy();
+	EBO1.destroy();
+	shaderProgram.destroy();
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
 }
---
+++ thirdparty/stb/stb.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liad on 22/01/2026.
+//
+#define STB_IMAGE_IMPLEMENTATION
+#include<stb/stb_image.h>
